1005766df98bb8d0d02d7ba09d2ec0a2b5b55ede
yousefjoe1 <yousefmahmoud150@gmail.com>
2026-01-20 00:30:26 +0200

    feat: Manually implement PWA features by adding a custom service worker and metadata, and removing the `next-pwa` package.

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,8 @@
 import type { NextConfig } from "next";
-import withPWAInit from "@ducanh2912/next-pwa";
-
-const withPWA = withPWAInit({
-  dest: "public",
-  disable: process.env.NODE_ENV === "development",
-  register: true,
-  // نقل الإعدادات التي تسببت في الخطأ إلى هنا إذا كنت تحتاجها
-  workboxOptions: {
-    skipWaiting: true,
-    clientsClaim: true,
-  },
-});
 
 const nextConfig: NextConfig = {
+  /* config options here */
   reactCompiler: true,
-  // أي إعدادات أخرى لـ Next.js تضعها هنا
 };
 
-export default withPWA(nextConfig);
+export default nextConfig;
