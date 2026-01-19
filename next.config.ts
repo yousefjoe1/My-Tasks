d484f4d2691d0ffbc5bd214a0cd85146fbc294b8
yousefjoe1 <yousefmahmoud150@gmail.com>
2026-01-20 00:21:29 +0200

    feat: Implement Progressive Web App (PWA) functionality by adding manifest, icons, and PWA configuration.

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,20 @@
 import type { NextConfig } from "next";
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  // نقل الإعدادات التي تسببت في الخطأ إلى هنا إذا كنت تحتاجها
+  workboxOptions: {
+    skipWaiting: true,
+    clientsClaim: true,
+  },
+});
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  // أي إعدادات أخرى لـ Next.js تضعها هنا
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
