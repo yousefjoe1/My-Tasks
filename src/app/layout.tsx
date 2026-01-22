6b1362d27fd6262fd72ba7a0133bcccc2029c40c
yousefjoe1 <yousefmahmoud150@gmail.com>
2026-01-22 16:57:04 +0200

    adding girls color mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import "./animate.css";
 import { ThemeProvider } from "@/contexts/ThemeContext";
 import { AuthProvider } from "@/contexts/AuthContext";
 
