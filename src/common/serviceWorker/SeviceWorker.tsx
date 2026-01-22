92d80e837ab8e176bde449771db264470a56e387
yousefjoe1 <yousefmahmoud150@gmail.com>
2026-01-22 09:17:33 +0200

    split service worker in it's comp update manifist images name to update the icon in the devices

---
+++ src/common/serviceWorker/SeviceWorker.tsx
@@ -0,0 +1,23 @@
+'use client';
+import { useEffect } from 'react'
+
+const SeviceWorker = () => {
+
+    useEffect(() => {
+        if ("serviceWorker" in navigator) {
+            window.addEventListener("load", () => {
+                navigator.serviceWorker
+                    .register("/sw.js")
+                    .then((registration) => console.log("SW registered: ", registration))
+                    .catch((registrationError) => console.log("SW registration failed: ", registrationError));
+            });
+        }
+    }, [])
+
+
+    return (
+        <div>serviceWorker</div>
+    )
+}
+
+export default SeviceWorker
