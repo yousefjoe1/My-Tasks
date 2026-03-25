403cb762e5ce105cb20d44afda7431bd0e182070
yousefjoe1 <yousefmahmoud150@gmail.com>
2026-03-25 16:22:39 +0200

    update vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -59,16 +59,6 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  // useEffect(() => {
-  //   if ("serviceWorker" in navigator) {
-  //     window.addEventListener("load", () => {
-  //       navigator.serviceWorker
-  //         .register("/sw.js")
-  //         .then((registration) => console.log("SW registered: ", registration))
-  //         .catch((registrationError) => console.log("SW registration failed: ", registrationError));
-  //     });
-  //   }
-  // }, []);
   return (
     <html lang="en">
       <body
